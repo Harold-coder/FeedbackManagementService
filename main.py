0cc501dfa5597d1f5b77da93a29f59ca0a72a31a
Ronit Kathuria <ronitkats15@gmail.com>
2023-11-29 00:23:41 -0500

    Feedack Management Working

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-from flask import Flask, render_template_string
+from flask import Flask, render_template, request, redirect, url_for
 import psycopg2
-import pandas as pd
-from credentials import PASSWORD, USER
+from datetime import datetime
 
-# Initialize the Flask app
 app = Flask(__name__)
 
 # Database connection function
@@ -11,48 +9,90 @@ def get_db_connection():
     connection = psycopg2.connect(
         host='db-cc.co4twflu4ebv.us-east-1.rds.amazonaws.com',
         port=5432,
-        user=USER,  # Use credentials imported from the credentials module
-        password=PASSWORD,
+        user='master',
+        password='MasterPassword',
         database='lion_leftovers'
     )
     return connection
 
-# HTML template for rendering the reviews table
-html_template = """
-<!doctype html>
-<html lang="en">
-<head>
-  <meta charset="utf-8">
-  <title>Reviews Table</title>
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css">
-</head>
-<body>
-  <div class="container">
-    <h1>Reviews Table</h1>
-    {{ reviews_table|safe }}
-  </div>
-</body>
-</html>
-"""
-
 @app.route("/")
-def hello():
-    """Return a friendly HTTP greeting."""
-    return "Hello World! This is Order Management Service"
+def index():
+    return render_template('index.html')
 
-@app.route("/reviews")
-def show_reviews():
-    """Display the reviews in an HTML table."""
-    # Get a database connection
-    connection = get_db_connection()
-    sql = "SELECT * FROM Reviews;"
-    # Read the SQL query into a Pandas DataFrame
-    reviews_df = pd.read_sql(sql, con=connection)
-    # Convert the DataFrame to HTML table representation
-    reviews_table = reviews_df.to_html(classes='table table-striped', index=False)
-    connection.close()  # Close the database connection
-    # Render the HTML page with the reviews table
-    return render_template_string(html_template, reviews_table=reviews_table)
 
-if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=8080, debug=True)
+@app.route('/student_reviews')
+def student_reviews():
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM Reviews") 
+    reviews = cursor.fetchall()
+    # cursor.close()
+    # conn.close()
+    return render_template('student_reviews.html', reviews=reviews)
+
+@app.route('/add_review', methods=['GET', 'POST'])
+def add_review():
+    if request.method == 'POST':
+        conn = get_db_connection()
+        cursor = conn.cursor()
+
+        student_uni = request.form['student_uni']
+        order_id = request.form['order_id']
+        rating = request.form['rating']
+        comment = request.form['comment']
+        review_time = datetime.now()  
+
+        cursor.execute("INSERT INTO Reviews (StudentUNI, OrderID, Rating, Comment, ReviewTime) VALUES (%s, %s, %s, %s, %s)",
+                       (student_uni, order_id, rating, comment, review_time))
+        conn.commit()
+        cursor.close()
+        conn.close()
+        return redirect(url_for('student_reviews'))
+
+    return render_template('add_review.html')
+
+@app.route('/edit_review/<int:review_id>', methods=['GET', 'POST'])
+def edit_review(review_id):
+    conn = get_db_connection()
+    cursor = conn.cursor()
+
+    if request.method == 'POST':
+        rating = request.form['rating']
+        comment = request.form['comment']
+        updated_time = datetime.now()  # Get the current date and time
+
+        cursor.execute("UPDATE Reviews SET Rating = %s, Comment = %s, ReviewTime = %s WHERE ReviewID = %s",
+                       (rating, comment, updated_time, review_id))
+        conn.commit()
+        cursor.close()
+        conn.close()
+        return redirect(url_for('student_reviews'))
+
+    cursor.execute("SELECT * FROM Reviews WHERE ReviewID = %s", (review_id,))
+    review = cursor.fetchone()
+    cursor.close()
+    conn.close()
+    return render_template('edit_review.html', review=review)
+
+@app.route('/manage_reviews')
+def manage_reviews():
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM Reviews")
+    reviews = cursor.fetchall()
+    cursor.close()
+    conn.close()
+    return render_template('manage_reviews.html', reviews=reviews)
+
+@app.route('/delete_review/<int:review_id>', methods=['POST'])
+def delete_review(review_id):
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute("DELETE FROM Reviews WHERE ReviewID = %s", (review_id,))
+    conn.commit()
+    cursor.close()
+    conn.close()
+    return redirect(url_for('manage_reviews'))
+
+if __name__ == '__main__':
+    app.run(debug=True, port=8012)
